fdbde68e9f9cdf5a17b0b8e0e17503883a86dbca
Chad Fawcett <me@chadf.ca>
2016-06-22 14:58:26 -0700

    Change isValid to isRange and include results

--- src/parser.js
+++ src/parser.js
@@ -14,9 +14,9 @@ parser.refiners.push(dayOverlapRefiner)
 
 export default function(str, ref) {
   let rslt = parser.parse(str, ref)[0]
-  let isValid = rslt && rslt.start && rslt.end
-  let start = isValid ? rslt.start.date() : null
-  let end = isValid ? rslt.end.date() : null
-  let text = isValid ? rslt.text : null
-  return { start, end, text, isValid, }
+  let isRange = rslt && rslt.start && rslt.end
+  let start = rslt.start ? rslt.start.date() : null
+  let end = rslt.end ? rslt.end.date() : null
+  let text = rslt.text
+  return { start, end, text, message, isRange, }
 }
